b97def73e20aaa86225c6bea3c9074cfb7bf5900
nobiart <dmitri.besleaga@gmail.com>
2024-11-08 23:32:16 +0200

    add _page, per_page, and more items to DB

--- src/modules/todo-list/TodoList.tsx
+++ src/modules/todo-list/TodoList.tsx
@@ -1,10 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api.ts";
+import { useState } from "react";
 
 export const TodoList = () => {
-  const { data, error, isPending } = useQuery({
-    queryKey: ["tasks", "list"],
-    queryFn: todoListApi.getTodoList,
+  const [page, setPage] = useState(1);
+
+  const { data, error, isPending, isPlaceholderData } = useQuery({
+    queryKey: ["tasks", "list", { page }],
+    queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
+    placeholderData: keepPreviousData,
   });
 
   if (isPending) {
@@ -16,11 +20,33 @@ export const TodoList = () => {
   }
 
   return (
-    <div>
-      To Do List
-      {data.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
-      ))}
+    <div className="my-10 px-5 mx-auto max-w-[1200]">
+      <h1 className="text-3xl font-bold mb-5">To Do List</h1>
+      <div
+        className={
+          "flex flex-col gap-4" + (isPlaceholderData ? " opacity-50" : "")
+        }
+      >
+        {data?.data.map((todo) => (
+          <div key={todo.id} className="p-3 border border-slate-500 rounded-md">
+            {todo.text}
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-3 mt-5">
+        <button
+          className="px-3 py-2 rounded-md border border-orange-400"
+          onClick={() => setPage(Math.max(page - 1, 1))}
+        >
+          Prev
+        </button>
+        <button
+          className="px-3 py-2 rounded-md border border-orange-400"
+          onClick={() => setPage(Math.min(page + 1, data?.items))}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -6,10 +6,23 @@ export type TodoDto = {
   done: boolean;
 };
 
+export type PaginatedResult<T> = {
+  data: T[];
+  first: number | null;
+  items: number;
+  last: number | null;
+  next: number | null;
+  pages: number;
+  prev: number | null;
+};
+
 export const todoListApi = {
-  getTodoList: ({ signal }: { signal: AbortSignal }) => {
-    return fetch(`${BASE_URL}/tasks`, { signal }).then(
-      (res) => res.json() as Promise<TodoDto[]>,
-    );
+  getTodoList: (
+    { page }: { page: number },
+    { signal }: { signal: AbortSignal },
+  ) => {
+    return fetch(`${BASE_URL}/tasks?_page=${page}&_per_page=10`, {
+      signal,
+    }).then((res) => res.json() as Promise<PaginatedResult<TodoDto>>);
   },
 };
