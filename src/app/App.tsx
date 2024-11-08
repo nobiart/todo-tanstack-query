061a22ccfbfe8d619a663abc6a32003fbbc02b8a
nobiart <dmitri.besleaga@gmail.com>
2024-11-08 23:29:56 +0200

    add react-query-devtools

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/queryClient.ts";
 import { TodoList } from "../modules/todo-list/TodoList.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TodoList />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
