39305bd0e768f90a5cdf7ecca0ab69db3c65873a
nobiart <dmitri.besleaga@gmail.com>
2024-11-17 20:40:49 +0200

    add redux toolkit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/queryClient.ts";
 import { TodoList } from "../modules/todo-list/TodoList.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+import { store } from "../shared/redux";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TodoList />
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
